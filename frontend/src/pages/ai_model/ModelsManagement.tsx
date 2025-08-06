f728caeef07905f70bb6825fd4ae8f8897601845
gaochao <18853503922@163.com>
2025-08-06 12:58:41 +0800

    xxx

--- frontend/src/pages/ai_model/ModelsManagement.tsx
+++ frontend/src/pages/ai_model/ModelsManagement.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useTheme } from '../../../hooks/ThemeContext';
+import { useTheme } from '../../hooks/ThemeContext';
 import { 
   Card, 
   Row, 
