155b2826e2c397b6d9d2214e74eb52b0c341416d
gaochao <18853503922@163.com>
2025-09-22 09:38:59 +0800

    xxx

--- backend/init_fast.sql
+++ backend/init_fast.sql
@@ -1,3 +1,3 @@
 # 备份语句
 /home/mysql/multi/3306/mysql57/bin/mysqldump -h127.0.0.1 -uroot -pfffjjj --set-gtid-purged=OFF --single-transaction --master-data=2 --skip-tz-utc --default-character-set=utf8mb4 -d omind >omind_prd_frm.sql
-/home/mysql/multi/3306/mysql57/bin/mysqldump -h127.0.0.1 -uroot -pfffjjj --set-gtid-purged=OFF --single-transaction --master-data=2 --skip-tz-utc --default-character-set=utf8mb4 omind_prd rbac_roles rbac_menus rbac_permissions >omind_prd_data.sql
+/home/mysql/multi/3306/mysql57/bin/mysqldump -h127.0.0.1 -uroot -pfffjjj --set-gtid-purged=OFF --single-transaction --master-data=2 --skip-tz-utc --default-character-set=utf8mb4 --no-create-info --complete-insert omind rbac_roles rbac_menus rbac_permissions >omind_prd_data.sql
